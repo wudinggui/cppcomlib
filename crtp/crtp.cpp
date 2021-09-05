89b82d61f0fee5019a2e71e0ba1253ee556fd6f9
wudinggui <839275348@qq.com>
2021-09-05 14:55:50 +0800

    Update crtp.cpp

--- crtp/crtp.cpp
+++ crtp/crtp.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+template<typename T>
+class Base {
+public:
+    virtual ~Base() = default;
+    virtual void Interface() const = 0;
+};
+
+class Derived: public Base<Derived> {
+public:
+    void Interface() const {
+        cout << "Do Interface." << endl;
+    }
+};
+
+int main ()
+{
+    Base<Derived> *b = new Derived();
+    b->Interface();
+
+    return 0;
+}
